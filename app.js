d6416f2c282678d391a58d5bf06604158b3849db
Lars Thomsen <TheThomsen05@gmail.com>
2022-07-12 00:46:57 -0400

    Setup color change on mouseover event

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ let createDiv = () => document.createElement('div');
 for (i = 0; i < 16; i++){
     gameBoard.appendChild(createDiv());
     for (x = 0; x < 16; x++){
-        gameBoard.childNodes[i].appendChild(createDiv()).setAttribute('style', 'height: 20px; width: 20px; background-color: black')
+        gameBoard.childNodes[i].appendChild(createDiv()).setAttribute('class', 'cell')
     }
 }
 
-console.log(gameBoard.childNodes.length);
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach(cell => cell.addEventListener('mouseover', e => { 
+    e.target.setAttribute('style', 'background-color: black');
+}));
+
 
 //.appendChild(createDiv()).setAttribute('style', 'height: 20px; width: 20px')
