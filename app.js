0f65ab906c483990126128650d42fe60ca8c1566
Lars Thomsen <TheThomsen05@gmail.com>
2022-07-12 05:02:47 -0400

    Make amount of cells in grid ajustable

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const gameBoard = document.querySelector('#game-board');
+const slider = document.querySelector('#set-cells');
+const numberDisplay = document.querySelector('.number-display');
 
 let createDiv = () => document.createElement('div');
 
-for (i = 0; i < 16; i++){
-    gameBoard.appendChild(createDiv());
-    for (x = 0; x < 16; x++){
-        gameBoard.childNodes[i].appendChild(createDiv()).setAttribute('class', 'cell')
-    }
+let sliderValue = 16
+
+slider.oninput = (e) => {
+    sliderValue = e.target.value;
+    console.log(sliderValue);
+    setGrid();
+    numberDisplay.innerHTML = `${sliderValue} x ${sliderValue}`
 }
 
-const cells = document.querySelectorAll('.cell');
+function setGrid() {
+    //clear board
+    while(gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild);
+    };
+    //setup cells
+    for (i = 0; i < sliderValue; i++){
+        gameBoard.appendChild(createDiv());
+        for (x = 0; x < sliderValue; x++){
+            gameBoard.childNodes[i].appendChild(createDiv()).setAttribute('class', 'cell')
+        }
+    }
+    const cells = document.querySelectorAll('.cell');
+    //add event listerners to cells
+    cells.forEach(cell => cell.addEventListener('mouseover', e => { 
+        e.target.setAttribute('style', 'background-color: black');
+    }));
+}
 
-cells.forEach(cell => cell.addEventListener('mouseover', e => { 
-    e.target.setAttribute('style', 'background-color: black');
-}));
+setGrid()
 
 
 //.appendChild(createDiv()).setAttribute('style', 'height: 20px; width: 20px')
